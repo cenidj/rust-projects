fe1bd104a9817edc4cb69192ea35e529fa373a0d
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-03 15:52:21 -0400

    added comments to the hello world program

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -1,3 +1,6 @@
-fn main() {
-  println!("Hello World!");
+fn main() { /* main fn always execute first, it should exists in every rust program */
+    println!("Hello World!");
 }
+
+// ! it's to indicates is a macro not a function.
+// if you want to call a function, you should not use the !

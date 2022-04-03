064f223415e536603712118d512283291ac4c508
Cesario Nivar <ing.cesarionivar@gmail.com>
2022-04-03 19:17:08 -0400

    shadowing

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+    let x = 5;
 
-    println!("{}", THREE_HOURS_IN_SECONDS);
+    let x = x + 1;
+
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {}", x);
+    }
+
+    println!("The value of x is: {}", x);
 }
